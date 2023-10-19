cc8cf1e0e15d0cff62c39d5fee11c8f4d76799f9
Your Name <you@example.com>
2023-10-19 14:09:44 +0500

    yangi file yaratdim

---
+++ main.js
@@ -0,0 +1,64 @@
+let qator1 =                      __________________________________________________________
+let qator2 =   "                 ||--------------------------------------------------------||                                                               "
+let qator3 =  '                  ||                                                        ||       ________________                                                          '
+let qator4 =  "                  ||                                                        ||     ||                ||                                     "
+let qator5 =  "                  ||                                                        ||     ||     *    *     ||                                        "
+let qator6 =  "                  ||                                                        ||     ||                ||                                                          "
+let qator7 = "                   ||                      TEXNOPARK                         ||     ||    ~~~~~~~~    ||                                                           "
+let qator8 = "                   ||                      PROGRAMMER                        ||     ||                ||                                                           "
+let qator9= "                    ||                       2008-SILA                        ||     ||    ||||||||    ||                                                          "
+let qator10= "                   ||                                                        ||     ||                ||                                                              "
+let qator11= "                   ||                                                        ||     ||    --------    ||                                                            "
+let qator12= "                   ||                                                        ||     ||                ||                                                          "
+let qator13= "                   ||                                                        ||     ||   [........]   ||                                                             "
+let qator14= "                   ||_________________________[_]____________________________||     ||                ||                                                            "
+let qator15="                    ||________________________________________________________||     ||                ||                                                             "
+let qator16="                                            |       |                                ||                ||                                                           "
+let qator17="                                  _____________________________                      ||   [<<<<>>>>]   ||                                                                                        "
+let qator18= "                                                                                    ||                ||                                                           "
+let qator19= "                                                                                    ||                ||                                                   "
+let qator21= "                      \ ~''''~''''~''''~''''~''''~''''~''''~''''~''''~''''~\        ||________________||                                                                                                        "
+let qator22=  "                      \''''''''''''''''''''''''''''''''''''''''''''''''''''\                 (                                                "  
+let qator23="                         \''''''''''''''''''''''''''''''''''''''''''''''''''''\                 )                                              "
+let qator24="                          \''''''''''''''''''''''''''''''''''''''''''''''''''''\               (                                               "
+let qator25="                           \''''''''''''''''''''____________''''''''''''''''''''\               )                                               "
+let qator26="                            \                                                    \             (                                             "
+let qator27="                             \____________________________________________________\           (|)                                          "
+
+
+
+
+
+
+
+
+
+
+
+
+console.log(qator2);
+console.log(qator3);
+console.log(qator4);
+console.log(qator5);
+console.log(qator6);
+console.log(qator7);
+console.log(qator8);
+console.log(qator9);
+console.log(qator10);
+console.log(qator11);
+console.log(qator12);
+console.log(qator13);
+console.log(qator14);
+console.log(qator15);
+console.log(qator16);
+console.log(qator17);
+console.log(qator18);
+console.log(qator19);
+console.log(qator20);
+console.log(qator21);
+console.log(qator22);
+console.log(qator23);
+console.log(qator24);
+console.log(qator25);
+console.log(qator26);
+console.log(qator27);
